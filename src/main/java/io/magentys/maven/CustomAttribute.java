57a5335404392354e52cb4504468cd07e0390c48
Dave Bassan <dave.bassan@magentys.co.uk>
2016-11-29 12:09:04 +0000

    add support for custom attributes

---
+++ src/main/java/io/magentys/maven/CustomAttribute.java
@@ -0,0 +1,23 @@
+package io.magentys.maven;
+
+public class CustomAttribute {
+
+    private String name;
+    private String value;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
